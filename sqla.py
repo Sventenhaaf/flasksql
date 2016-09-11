af64bd8d8b17010ef2a39d6200f10a85a09887ff
Sven ten Haaf <sventenhaaf@hotmail.com>
2016-09-11 16:23:14 +0200

    some sql exercises

---
+++ sqla.py
@@ -0,0 +1,23 @@
+# Create a SQLite3 database and table
+
+
+# import the sqlite3 library
+import sqlite3
+
+# create a new database if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE population
+                (city TEXT, state TEXT, population INT)
+                """)
+
+cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco', 'SF', 800000)")
+
+conn.commit()
+# close the database connection
+conn.close()
